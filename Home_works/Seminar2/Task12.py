d2e99326c21099eb202067366ed6426707244e61
A9219 <alexey1978malyshev@gmail.com>
2023-05-23 20:19:18 +0300

    task12 comments added

--- Home_works/Seminar2/Task12.py
+++ Home_works/Seminar2/Task12.py
@@ -8,8 +8,7 @@
 # 4 4 -> 2 2
 # 5 6 -> 2 3
 
-# S = X + Y
-# P = X * Y
+import math
 
 summa = int(input('Введите сумму: '))
 multiple = int(input('Введите произведение: '))
@@ -17,11 +16,25 @@ multiple = int(input('Введите произведение: '))
 result_1 = 0
 result_2 = 0
 
-for i in range(summa):
-    for j in range(summa):
-        if i * j == multiple and i + j == summa:
-            result_1 = i
-            result_2 = j
-            break
+# for i in range(summa):
+#     for j in range(summa):
+#         if i * j == multiple and i + j == summa:
+#             result_1 = i
+#             result_2 = j
+#             break
+
+
+# ну или математически), сложность будет константная, вроде бы, или даже единичная... вместо квадратичной
+# summa = result_1 + result_2
+# multiple = result_1 * result_2
+# result_1 = summa - result_2
+# result_1 = multiple / result_2 ==>
+# ==> summa - result_2 = multiple / result_2 ==>
+# ==> multiple = result_2 * (summa - result_2) ==>
+# ==> -result_2 **2 + result_2 * summa - multiple = 0  сводим всё безобразие к квадратному уравнению
+
+d = math.pow(summa, 2) - 4 * -1 * -multiple  # находим дискриминант
+result_2 = (-summa + math.sqrt(d)) / 2 * -1  # и корень... хватит одного
+result_1 = summa - result_2
 
 print(f'{result_1} **** {result_2}')
