5a3c95a82974c5d84782a52884e9d99ffe83e63e
A9219 <alexey1978malyshev@gmail.com>
2023-05-26 17:16:15 +0300

    resolve corrected

--- Home_works/Seminar3/Task18.py
+++ Home_works/Seminar3/Task18.py
@@ -8,17 +8,26 @@
 # Output: 2
 # Input: [10, 5, 7, 3, 3, 2, 5, 7, 3, 8], X = 9
 # Output: 10
+#(*) Усложнение. Если в списке несколько чисел "равноблизких" к заданному числу X, выводим все числа в отсортированном виде (по возрастанию)
 import random
 
 num_list = [random.randint(i, 99) for i in range(1, 11)]
 print(num_list)
-#num_list = [10, 5, 7, 3, 3, 2, 5, 7, 3, 8]
+#num_list = [10, 5, 7, 3, 3, 2, 5, 7, 4, 3, 8]
+
 x = int(input('Введите число: '))
 
 closest_elem = num_list[0]
+lst_of_closest = []
 
 for i in num_list:
-    if abs(i - x) < abs(closest_elem - x):
-        quiet_elem = i
+    if abs(i - x) < abs(closest_elem - x) and abs(i - x) != 0:
+        closest_elem = i
+
+for k in num_list:
+    if abs(k - x) == abs(closest_elem - x):
+        lst_of_closest.append(k)
+lst_of_closest.sort()
 
 print(closest_elem)
+print(lst_of_closest)
